55df73254c54208a14eb1a6b51900dc6ad61aea5
denis.niwemugisha <denis.niwemugisha@codeland.it>
2021-07-15 22:28:51 +0200

    set up env

--- src/database/config.js
+++ src/database/config.js
@@ -1,18 +1,35 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv'
+import env from './config.json'
+dotenv.config()
+const envVar = process.env.NODE_ENV
+const config = () => {
+      let environment ={}
+      if ( envVar === 'docker' ){
+        environment.username = process.env.POSTGRES_USER
+        environment.password = process.env.POSTGRES_PASSWORD
+        environment.database = process.env.POSTGRES_DB
+        environment.host = process.env.DB_HOST
+        environment.dialect = process.env.DB_DIALECT
+       
+      }
 
-dotenv.config();
+      else if(envVar === 'production'){
+        environment.use_env_variable="DATABASE_URL",
+        environment.dialect='postgres',
+        environment.dialectOptions= {
+          ssl: {      /* <----- Add SSL option */
+            require: true,
+            rejectUnauthorized: false 
+          }
+        }
+      }
 
-module.exports = {
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-  },
-  development: {
-    use_env_variable: 'DEV_DATABASE_URL',
-    dialect: 'postgres',
-  },
-  test: {
-    use_env_variable: 'DATABASE_URL_TEST',
-    dialect: 'postgres',
-  },
-};
+      else 
+      {
+        console.log(`Connecting to ${envVar} db`)
+        environment = env[envVar]
+      }
+        return environment
+  }
+
+module.exports= config;
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,23 +1,42 @@
+'use strict';
+
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-import configFile from '../config';
-
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = configFile[env];
+import configurations from  '../config.js'
 const db = {};
-config.logging = false;
-const sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
-fs.readdirSync(__dirname)
-  .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+console.log("Database connecting to <==>",process.env.NODE_ENV)
+let config = configurations()
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+console.log("authenticating ...")
+sequelize
+  .authenticate()
+    .then(() => {
+      console.log('Database Connection has been established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -26,4 +45,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
